1fd703f7df6d3700ef90e120a708b2faeeff51d3
SAAKSHI2 <saakshibhandari27@gmail.com>
2021-07-20 10:58:58 +0530

    26th commit

---
+++ Problem_Solving/Breaking_the_Records.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'breakingRecords' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY scores as parameter.
+     */
+
+    public static List<Integer> breakingRecords(List<Integer> scores) {
+    // Write your code here
+    int max=scores.get(0),min=scores.get(0);
+    List<Integer> s = new ArrayList<>();
+    s.add(0);
+    s.add(0);
+    for(int i=1;i<scores.size();i++)
+    {
+        if(scores.get(i)>max)
+        {
+            s.set(0,s.get(0)+1);
+            max = scores.get(i);
+        }
+        if(scores.get(i)<min)
+        {
+            s.set(1,s.get(1)+1);
+            min=scores.get(i);
+        }
+    }
+    return s;
+    
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> scores = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.breakingRecords(scores);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}

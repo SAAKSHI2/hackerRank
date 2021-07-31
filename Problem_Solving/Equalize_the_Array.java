aa38a42a392510e173783a38a8f68fbeb87c0ed8
SAAKSHI2 <saakshibhandari27@gmail.com>
2021-07-31 21:08:42 +0530

    59th commit

---
+++ Problem_Solving/Equalize_the_Array.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'equalizeArray' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int equalizeArray(List<Integer> arr) {
+    // Write your code here
+    Collections.sort(arr);
+    int c=0,cc=0,max=Integer.MIN_VALUE;
+    for(int i=0;i<arr.size();)
+    {
+        cc=0;
+        int j=i;
+        for(j=i;j<arr.size();j++)
+        {
+            if(arr.get(j)==arr.get(i))
+            cc++;
+            else
+            break;
+        }
+        if(cc>max)
+        max=cc;
+        
+        i=j;
+        
+    }
+    
+    return arr.size()-max;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.equalizeArray(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}

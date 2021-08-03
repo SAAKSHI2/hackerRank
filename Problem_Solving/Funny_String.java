e61dab563b70459963cafbf163ab7c081e890c55
SAAKSHI2 <saakshibhandari27@gmail.com>
2021-08-03 10:09:09 +0530

    71th commit

---
+++ Problem_Solving/Funny_String.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'funnyString' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String funnyString(String s) {
+    // Write your code here
+    StringBuffer sb = new StringBuffer(s);
+    sb.reverse();
+    int a[]=new int[s.length()];
+    int b[]=new int[s.length()];
+    String ss=sb.toString();
+    for(int i=1;i<s.length();i++)
+    {
+        a[i-1]=Math.abs((int)(s.charAt(i)-s.charAt(i-1)));
+        b[i-1]=Math.abs((int)(ss.charAt(i)-ss.charAt(i-1)));
+    }
+    if(Arrays.equals(a,b))
+    return "Funny";
+    return"Not Funny";
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                String result = Result.funnyString(s);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}

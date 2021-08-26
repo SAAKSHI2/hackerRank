ce26451b8148166cb99dce11b3ad78b5c82fc30e
SAAKSHI2 <saakshibhandari27@gmail.com>
2021-08-26 19:30:51 +0530

    90th commit

---
+++ Problem_Solving/2D_Array_DS.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'hourglassSum' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int hourglassSum(List<List<Integer>> arr) {
+    // Write your code here
+    int sum=0,max=Integer.MIN_VALUE;
+  for(int i=0;i<=arr.size()/2;i++)
+  {
+      for(int j=0;j<=arr.size()/2;j++)
+      {
+          sum=0;
+          for(int k=0;k<3;k++)
+          {
+              sum+=arr.get(i).get(j+k)+arr.get(i+2).get(j+k);
+          }
+          sum+=arr.get(i+1).get(j+1);
+          if(sum>max)
+          max=sum;
+      }
+  }
+  return max;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, 6).forEach(i -> {
+            try {
+                arr.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = Result.hourglassSum(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
